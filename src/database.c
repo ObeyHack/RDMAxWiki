ae8c6de278f5bdd07d0f4e1ac865f886ed8bf03a
sean.cherny <sean.cherny@mail.huji.ac.il>
2024-07-31 12:22:52 +0300

    db set

--- src/database.c
+++ src/database.c
@@ -10,21 +10,36 @@ bool create_database(Database** db_p){
     return EXIT_SUCCESS;
 }
 
-bool set_item(Database* db, char* key, char* value){
-    Item* item = (Item*)malloc(sizeof(Item));
-    if (item == NULL){
+bool add_item(Database* db, char* key, char* value) {
+    Item *item = (Item *) malloc(sizeof(Item));
+    if (item == NULL) {
         return EXIT_FAILURE;
     }
     item->key = key;
     item->value = value;
-    Node* node = (Node*)malloc(sizeof(Node));
-    if (node == NULL){
+    Node *node = (Node *) malloc(sizeof(Node));
+    if (node == NULL) {
         return EXIT_FAILURE;
     }
     node->data = item;
     node->next = db->head;
     db->head = node;
     return EXIT_SUCCESS;
+}
+
+
+bool set_item(Database* db, char* key, char* value){
+    // first check if the key already exists
+    Node* current = db->head;
+    while (current != NULL){
+        if (strcmp(current->data->key, key) == 0){
+            current->data->value = value;
+            return EXIT_SUCCESS;
+        }
+        current = current->next;
+    }
+    // if the key does not exist, add it
+    return add_item(db, key, value);
 }
 
 bool get_value(Database* db, char* key, char** value_p){
