a28682c813a3849f9e82121de5d7755537bfaf12
sean.cherny <sean.cherny@mail.huji.ac.il>
2024-07-31 10:59:22 +0300

    client c

---
+++ src/client.c
@@ -0,0 +1,23 @@
+#include "client.h"
+
+int kv_open(char *servername, void **kv_handle){
+    return 0;
+}
+
+int kv_set(void *kv_handle, const char *key, const char *value){
+    return 0;
+}
+
+int kv_get(void *kv_handle, const char *key, char **value){
+    return 0;
+}
+
+/* Called after get() on value pointer */
+void kv_release(char *value){
+    return;
+}
+
+/* Destroys the QP */
+int kv_close(void *kv_handle){
+    return 0;
+}
